b92b4eb948a1bc0084b248ec3f89ac2279962c8f
Theo-neuromedia <tlecardonnel@neuromedia.io>
2023-08-16 17:53:29 -0400

    Add markers

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,4 @@
+import { LatLng, LatLngBounds } from "leaflet";
 import * as L from "leaflet-gpx";
 
 async function renderMap() {
@@ -7,27 +8,38 @@ async function renderMap() {
     "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   ).addTo(map);
 
-  ['Albany_Chicago', 'Chicago_Seattle', 'Galesburg_SF', 'LA_Galesburg', 'LA_NO', 'MTL_NYC', 'NO_NYC', 'Seattle_LA'].forEach(
-    async (name) => {
-      const gpxString = await fetch(`https://raw.githubusercontent.com/tlecardo/testGPX/main/${name}.gpx`).then((res) =>
-      res.text()
-    );
-    
-    new L.GPX(gpxString, {
-      async: true,
-      marker_options: {
-        startIconUrl: '',
-        endIconUrl: '',
-        shadowUrl: ''
+  L.marker([42.641298, -73.741554]).bindTooltip("Albany", { direction: 'right' }).addTo(map);
+  L.marker([41.875152, -87.632273]).bindTooltip("Chicago", { direction: 'right' }).addTo(map);
+  L.marker([47.597811, -122.329564]).bindTooltip("Seattle", { direction: 'right' }).addTo(map);
+  L.marker([37.840341, -122.292293]).bindTooltip("San Fransisco", { direction: 'right' }).addTo(map);
+  L.marker([40.944502, -90.363511]).bindTooltip("Galesburg", { direction: 'right' }).addTo(map);
+  L.marker([34.055863, -118.234245]).bindTooltip("Los Angeles", { direction: 'right' }).addTo(map);
+  L.marker([29.946275, -90.078913]).bindTooltip("La Nouvelle-Orléans", { direction: 'right' }).addTo(map);
+  L.marker([40.750262, -73.992824]).bindTooltip("New York", { direction: 'right' }).addTo(map);
+  L.marker([45.500295, -73.567149]).bindTooltip("Montréal", { direction: 'right' }).addTo(map);
+
+  map.fitBounds(new LatLngBounds(new LatLng(32, -122.292293), new LatLng(45.500295, -73.567149)));
+
+  ['Albany_Chicago', 'Chicago_Seattle',
+    'Galesburg_SF', 'LA_Galesburg',
+    'LA_NO', 'MTL_NYC',
+    'NO_NYC', 'Seattle_LA']
+    .forEach(
+      (name) => {
+        console.log(name)
+        fetch(`https://raw.githubusercontent.com/tlecardo/testGPX/main/files/${name}.gpx`)
+          .then(res => res.text())
+          .then(res => {
+            new L.GPX(res, {
+              async: true,
+              marker_options: { startIconUrl: '', endIconUrl: '', shadowUrl: '' }
+            }).on("loaded", (e) => {
+              var gpx = e.target;
+            }).addTo(map);
+          })
       }
-    })
-      .on("loaded", (e) => {
-        var gpx = e.target;
-        map.fitBounds(gpx.getBounds());
-      })
-      .addTo(map);
-    }
-  )
+    )
+
 }
 
 renderMap().catch(console.error);
