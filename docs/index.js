9c6a3f59e2ab1252f8344a5fa988fb391695c8c2
Theo-neuromedia <tlecardonnel@neuromedia.io>
2023-08-18 10:13:14 -0400

    Rename file

--- docs/index.js
+++ docs/index.js
@@ -11,7 +11,7 @@ async function renderMap() {
   L.marker([42.641298, -73.741554]).bindTooltip("Albany", { direction: 'right' }).addTo(map);
   L.marker([41.875152, -87.632273]).bindTooltip("Chicago", { direction: 'right' }).addTo(map);
   L.marker([47.597811, -122.329564]).bindTooltip("Seattle", { direction: 'right' }).addTo(map);
-  L.marker([37.840341, -122.292293]).bindTooltip("San Fransisco", { direction: 'right' }).addTo(map);
+  L.marker([37.840341, -122.292293]).bindTooltip("San Francisco", { direction: 'right' }).addTo(map);
   L.marker([40.944502, -90.363511]).bindTooltip("Galesburg", { direction: 'right' }).addTo(map);
   L.marker([34.055863, -118.234245]).bindTooltip("Los Angeles", { direction: 'right' }).addTo(map);
   L.marker([29.946275, -90.078913]).bindTooltip("La Nouvelle-Orléans", { direction: 'right' }).addTo(map);
@@ -20,26 +20,26 @@ async function renderMap() {
 
   map.fitBounds(new LatLngBounds(new LatLng(32, -122.292293), new LatLng(45.500295, -73.567149)));
 
-  ['Albany_Chicago', 'Chicago_Seattle',
-    'Galesburg_SF', 'LA_Galesburg',
-    'LA_NO', 'MTL_NYC',
-    'NO_NYC', 'Seattle_LA']
+  ['Lake_Shore_Limited', 'Empire_Builder',
+    'California_Zephyr', 'Texas_Eagle',
+    'Sunset_Limited', 'Adirondack',
+    'Crescent', 'Coast_Starlight']
     .forEach(
       (name) => {
-        console.log(name)
         fetch(`https://raw.githubusercontent.com/tlecardo/testGPX/main/files/${name}.gpx`)
           .then(res => res.text())
-          .then(res => {
+          .then(async res => {
             new L.GPX(res, {
               async: true,
               marker_options: { startIconUrl: '', endIconUrl: '', shadowUrl: '' }
-            }).on("loaded", (e) => {
-              var gpx = e.target;
-            }).addTo(map);
+            }).bindTooltip(`${name.replaceAll("_", " ")}`, { direction: 'right' }).addTo(map);
           })
       }
     )
 
+  console.log(map)
+
 }
+
 
 renderMap().catch(console.error);
