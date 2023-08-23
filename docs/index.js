5725bc1692ff028d7bcc1a95cd9ae5c2184cf4f0
Theo-neuromedia <tlecardonnel@neuromedia.io>
2023-08-23 15:27:10 -0400

    Change tracks

--- docs/index.js
+++ docs/index.js
@@ -20,31 +20,48 @@ async function renderMap() {
 
   map.fitBounds(new LatLngBounds(new LatLng(32, -122.292293), new LatLng(45.500295, -73.567149)));
 
-  ['Lake_Shore_Limited', 'Empire_Builder',
-    'California_Zephyr', 'Texas_Eagle',
-    'Sunset_Limited', 'Adirondack',
-    'Crescent', 'Coast_Starlight']
-    .forEach(
-      (name) => {
-        fetch(`https://raw.githubusercontent.com/tlecardo/testGPX/main/files/${name}.gpx`)
-          .then(res => res.text())
-          .then(res => {
-            let time = res.match(/time = [0-9]*h [0-9]*m/)[0]
-              .replace("time = ", "")
-            
-            let dist = res.match(/track-length = [0-9]* filtered/)[0]
-              .replace("track-length = ", "")
-              .replace(" filtered", "")
-            dist = Math.round(dist/100)/10
-            dist = parseInt( dist ).toLocaleString()
-            
-            new L.GPX(res, {
-              async: true,
-              marker_options: { startIconUrl: '', endIconUrl: '', shadowUrl: '' }
-            }).bindTooltip(`<div class="track title">${name.replaceAll("_", " ")}</div><div class="track info">${dist} kms</div><div class="track info">${time}</div>`).addTo(map);
-          })
-      }
-    )
+  let tracks = {}
+  let namesTracks = ['Lake_Shore_Limited', 'Empire_Builder', 'California_Zephyr', 'Texas_Eagle',
+    'Sunset_Limited', 'Adirondack', 'Crescent', 'Coast_Starlight']
+
+  for await (let name of namesTracks) {
+    await fetch(`https://raw.githubusercontent.com/tlecardo/testGPX/main/files/${name}.gpx`)
+      .then(res => res.text())
+      .then(res => {
+
+        let time = res.match(/time = [0-9]*h [0-9]*m/)[0]
+          .replace("time = ", "")
+
+        let dist = res.match(/track-length = [0-9]* filtered/)[0]
+          .replace("track-length = ", "")
+          .replace(" filtered", "")
+
+        dist = Math.round(dist / 100) / 10
+        dist = parseInt(dist).toLocaleString()
+
+        tracks[name] = res.match(/<trkpt lat="[\-0-9.]*" lon="[\-0-9.]*">/gm).map(x => x.match(/[\-0-9]+.[0-9]+/gm))
+
+        new L.GPX(res, {
+          async: true,
+          marker_options: { startIconUrl: '', endIconUrl: '', shadowUrl: '' }
+        }).bindTooltip(`<div class="track title">${name.replaceAll("_", " ")}</div><div class="track info">${dist} kms</div><div class="track info">${time}</div>`).addTo(map);
+      })
+  }
+
+  /*
+  let localPts = tracks["Empire_Builder"]
+  let i = 20
+  var myPositionMarker = L.marker(localPts[i]).addTo(map);
+
+  function myLoop() {
+    setTimeout(function () {
+      myPositionMarker.setLatLng(localPts[i])
+      i += 20;
+      myLoop();
+    }, 100)
+  }
+  myLoop();
+  */
 }
 
-renderMap().catch(console.error);
+renderMap().catch(console.error)
