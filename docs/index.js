a15fbd53fb4b4531c23fa6d3a52a7a8b6fb84989
Théo.L <lecardot14@gmail.com>
2023-09-28 17:43:59 -0400

    Change path status

--- docs/index.js
+++ docs/index.js
@@ -73,7 +73,7 @@ async function renderMap() {
       })
   }
 
-  let projTracks = ['Exo_1', 'Exo_2', 'Miami', 'Vancouver', 'Project']
+  let projTracks = ['Exo_1', 'Miami', 'Vancouver', 'Project']
   for await (let name of projTracks) {
     await fetch(`https://raw.githubusercontent.com/tlecardo/testGPX/main/files/Projected/${name}.gpx`)
       .then(res => res.text())
